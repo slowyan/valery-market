10c9b0c334f277d9296ce47465bf6e143e20d3b6
slowyan <yanshabalin@gmail.com>
2025-03-20 21:33:47 +0300

    improved adaptation to mobile devices

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'valery-market-server',
+      name: 'valery-market',
       script: './server/index.js',
-      env_production: {
+      env: {
         NODE_ENV: 'production',
         PORT: 5000
       },
-      instances: 'max',
-      exec_mode: 'cluster',
+      instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      env: {
+      env_production: {
         NODE_ENV: 'production'
       }
     }
